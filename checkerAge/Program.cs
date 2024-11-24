c6bc819c3cc07711b21e2985f8ec397219a4d7f8
Антон <antonnechlopochin@mail.ru>
2024-11-24 15:51:46 +0300

    feats: create new programm checker age with cycle While

---
+++ checkerAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace checkerAge;
+
+    class Program{
+        static void Main(string[] args){
+            while (true){
+                Console.Write("Введите ваш возраст: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age > 110 || age < 0){
+                    Console.WriteLine("Введите корректные данные.");
+                    continue;
+                }
+                else if (age >= 18){
+                    Console.WriteLine("Вход разрешён.");
+                    break;
+                }
+                else{
+                    Console.WriteLine("Доступ не разрешён. Вы не прошли проверку возраста, зайти на сайт можно только" + 
+                    $"через {18 - age}.");
+                    break;
+                }
+            }
+        }
+    }

f89fe81bc2a33323752b0224b1914c3ba28231e4
Антон <antonnechlopochin@mail.ru>
2024-11-24 15:55:47 +0300

    Revert "fix: add empry readline in checkerAge"
    This reverts commit 11e424789e4dbac6c1bd6cb20798b2b33217b605.

--- checkerAge/Program.cs
+++ checkerAge/Program.cs
@@ -10,6 +10,7 @@ namespace checkerAge;
                 int age = Convert.ToInt32(Console.ReadLine());
                 if (age > 110 || age < 0){
                     Console.WriteLine("Введите корректные данные.");
+                    Console.WriteLine("");
                     continue;
                 }
                 else if (age >= 18){
@@ -19,7 +20,6 @@ namespace checkerAge;
                 else{
                     Console.WriteLine("Доступ не разрешён. Вы не прошли проверку возраста, зайти на сайт можно только" + 
                     $"через {18 - age}.");
-                    Console.WriteLine("");
                     break;
                 }
             }

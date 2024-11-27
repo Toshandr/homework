f22e012913b7285647b96b3c8ff74eab05ed7683
Антон <antonnechlopochin@mail.ru>
2024-11-27 20:00:56 +0300

    jean

--- mmm/Program.cs
+++ mmm/Program.cs
@@ -5,20 +5,21 @@ namespace mmm;
 
 class Programm{
     static void Main(string[] args){
-        int lenghtSp = Convert.ToInt32(Console.Readline());
+        int lenghtSp = Convert.ToInt32(Console.ReadLine());
         string[] shoppingList = new string[lenghtSp];
         int k = 1;
         while (k <= shoppingList.Length){
 
             Console.Write($"Товар {k}: ");
-            shoppingList[0] = Console.ReadLine();
+            shoppingList[k-1] = Console.ReadLine();
             k++;
         }
-        int k=1;
+        k=1;
 
         Console.WriteLine("\nВаш шопинг лист:");
-        while(k <= shoppingList){
+        while(k <= shoppingList.Length){
             Console.WriteLine($"{k}. " + shoppingList[k-1]);
+            k++;
         }
     }
 

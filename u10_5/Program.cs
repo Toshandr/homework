1b4f64d19e89429f082f42bc1b548283cc4022c8
Антон <antonnechlopochin@mail.ru>
2024-11-29 20:13:32 +0300

    chenges 5 - zadacha

--- u10_5/Program.cs
+++ u10_5/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        int k = 0;
         int[] spisok = {1,-2,-3,4,5,-6,-7};
         for(int i = 0; i < spisok.Length; i++){
             if (spisok[i] < 0){
-                Console.WriteLine(spisok[i]);
-                }
+                k += 1;
+            }
         }
+        Console.WriteLine(k);
     }
 }
--- u10_5/obj/Debug/net9.0/u10_5.AssemblyInfo.cs
+++ u10_5/obj/Debug/net9.0/u10_5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("u10_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e41d11ece8db222c68986ef247f1eaa1a4eba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a53bcbacc153021312465fad3dc73a3a800071f")]
 [assembly: System.Reflection.AssemblyProductAttribute("u10_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("u10_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
